2ce1fe0c681528832ba04f10d1bad134956ad231
Boris A. Burkov <vasjaforutube@gmail.com>
2023-03-17 16:15:38 +0300

    Create key interest rate Russia, CBRF pipeline.

---
+++ src/key_interest_rate_ru_cbrf.py
@@ -0,0 +1,30 @@
+import sys
+import requests
+import asyncio
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import telegram
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+# Calender: https://www.cbr.ru/dkp/cal_mp/
+
+
+async def generate_telegram_message(tg_api_token):
+    # Initialize the bot with your API token
+    bot = telegram.Bot(token=tg_api_token)
+
+    await bot.send_message(chat_id='@MacroPulse', text="Test", parse_mode='html')
+
+
+if __name__ == '__main__':
+    tg_api_token = sys.argv[1]
+
+    try:
+        asyncio.run(generate_telegram_message(tg_api_token))
+    except KeyboardInterrupt:  # Ignore exception when Ctrl-C is pressed
+        pass
