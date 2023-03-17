307fd946118f7c945e9051ac6fcb239016a0f144
Boris A. Burkov <vasjaforutube@gmail.com>
2023-03-17 12:57:06 +0300

    Stub US inflation pipeline.

---
+++ src/inflation_us_bls.py
@@ -0,0 +1,24 @@
+import sys
+import requests
+import asyncio
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import telegram
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+def generate_telegram_message(tg_api_token):
+    pass
+
+
+if __name__ == '__main__':
+    tg_api_token = sys.argv[1]
+
+    try:
+        asyncio.run(generate_telegram_message(tg_api_token))
+    except KeyboardInterrupt:  # Ignore exception when Ctrl-C is pressed
+        pass
